deabcc1aac1d9870ea3da020709b6a0e46a54395
pottj <pott.ne.ja@gmail.com>
2023-01-20 21:30:48 +0100

    add scripts 02-04: defining loci & performing interaction tests

---
+++ scripts/02_getAssociatedLoci.R
@@ -0,0 +1,274 @@
+#' ---
+#' title: "Get associated loci"
+#' subtitle: "PCSK9 GWAMA sex-stratified"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' Define loci as in Wuttke publication. I modify a script of Katrin Horn to do this. 
+#' 
+#' # Initialize ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../SourceFile_forostar.R")
+.libPaths()
+setwd(paste0(projectpath_main,"/scripts/"))
+
+source("../helperFunctions/getSmallestDist.R")
+
+#' # Load data ####
+#' ***
+#' Load data and filter for suggestive significant and valid SNPs. Finally, merge all significant SNPs into one object. 
+ToDoList = data.table(NR = 1:8)
+
+ToDoList[,statistic := list.files(path = "../data/",pattern = "PCSK9")]
+ToDoList[,statistic_path := paste0("../data/",statistic)]
+
+ToDoList[,pheno := gsub("SumStat_","",statistic)]
+ToDoList[,pheno := gsub("_230120.txt.gz","",pheno)]
+
+dumTab = foreach(i = 1:dim(ToDoList)[1])%do%{
+  #i=1
+  myRow = ToDoList[i,]
+  message("Working on data ",myRow$pheno)
+  data = fread(myRow$statistic_path)
+  
+  message("    SNPs before filter: ",dim(data)[1])
+  data = data[invalidAssocs  == F & pval<=1e-6,]
+  message("    SNPs after filter: ",dim(data)[1])
+  
+  data[,chrPosPheno := paste(chr,bp_hg19,phenotype,sep=":")]
+  data
+}
+
+result.1 = rbindlist(dumTab)
+table(duplicated(result.1$chrPosPheno))
+table(result.1$phenotype,result.1$chr)
+
+#' # Get Top SNP per 500 kb range ###
+#' ***
+#' I want to reduce the list of associated SNPs by assigning SNPs to the top SNP of the region per phenotype. To do this, I will order the SNPs by their p-value, choose the SNP with lowest p-value as lead SNP, and assign all SNPs within 500 kb around the lead SNP to this region. This will be repeated until no SNPs can be assign, as the minimal pairwise distance is larger than 500 kb.  
+#' 
+subset1 = unique(result.1$phenotype)
+subset1
+
+result.2 = foreach(s1 = subset1) %do% {
+  # s1 = subset1[1]
+  subdata = copy(result.1)
+  subdata = subdata[phenotype == s1, ]
+  subset2 = unique(subdata$chr)
+  
+  result.22 = foreach(s2 = subset2) %do% {
+    # s2 = subset2[1]
+    subdata2 = copy(subdata)
+    subdata2 = subdata2[chr == s2, ]
+    
+    setkey(subdata2, bp_hg19)
+    
+    if(dim(subdata2)[1]<=1){
+      subdata2[, keep := T]
+      subdata2[, NR_SNPs := 0]
+    }else{
+      subdata2[, keep := NA]
+      subdata2[, NR_SNPs := as.numeric(NA)]
+      
+      smallestDist = getSmallestDist(subdata2[, bp_hg19])
+      while(smallestDist < 500000) {
+        minP = min(subdata2[is.na(keep), pval])
+        mybp_hg19 = subdata2[minP == pval & is.na(keep), bp_hg19]
+        if(length(mybp_hg19)>1){
+          mybp_hg19 = mybp_hg19[1]
+        }
+        subdata2[bp_hg19 == mybp_hg19, keep := T]
+        
+        #filter for SNPs that can stay within the set (outside the +- 500 kb range or keep==T)
+        myFilt = (subdata2[, bp_hg19] < (mybp_hg19 - 500000)) | 
+          (subdata2[, bp_hg19] > (mybp_hg19 + 500000)) | 
+          subdata2[, keep] 
+        myFilt[is.na(myFilt)] = FALSE
+        subdata2 = subdata2[myFilt == TRUE, ]
+        
+        subdata2[bp_hg19 == mybp_hg19, NR_SNPs := sum(myFilt==F)]
+        smallestDist = getSmallestDist(subdata2[, bp_hg19])
+      }
+      
+      #stopifnot(sum(is.na(subdata2[,keep])) <= 1)
+      subdata2[is.na(keep), NR_SNPs := 0]
+      subdata2[is.na(keep), keep := TRUE]
+    }
+    
+    subdata2
+  }
+  result.22 = rbindlist(result.22)
+  table(result.22$phenotype)
+  table(result.22$chr)
+  result.22
+}
+result.2 = rbindlist(result.2)
+table(result.2$phenotype,result.2$chr)
+
+#add regions
+result.2[, region_start := bp_hg19 - 500000]
+result.2[, region_end := bp_hg19 + 500000]
+result.2
+
+#' # Locus collapsing by phenotype ###
+#' ***
+#' I want to collapse loci if their regions are overlapping, although their base positions differ more then 500 kb. This could be the case in large associated regions. 
+#' 
+
+result.3 = foreach(s1 = subset1) %do% {
+  # s1 = subset1[1]
+  subdata = copy(result.2)
+  subdata = subdata[phenotype == s1, ]
+  subset2 = unique(subdata$chr)
+  
+  result.32 = foreach(s2 = subset2) %do% {
+    # s2 = subset2[1]
+    subdata2 = copy(subdata)
+    subdata2 = subdata2[chr == s2, ]
+    
+    setkey(subdata2, bp_hg19)
+    
+    if(dim(subdata2)[1]<=1){
+      subdata2[, keep := T]
+      subdata2[, region := 1]
+    }else{
+      subdata2[, keep := NA]
+      subdata2[, region := as.numeric(NA)]
+      
+      subdata2[1, region := 1]
+      foreach(l = c(2:nrow(subdata2))) %do% {
+        if (subdata2[l, region_start] <= subdata2[l-1, region_end]) {
+          subdata2[l, region := subdata2[l-1, region]]
+        } else { subdata2[l, region := subdata2[l-1, region] + 1]}
+      }
+      
+      #keep best SNPs per region
+      allRegions = unique(subdata2[, region])
+      
+      subresult = foreach(r = allRegions) %do% {
+        subdata.2 = copy(subdata2)
+        subdata.2 = subdata.2[region == r, ]
+        minP = min(subdata.2[, pval])
+        subdata.2[pval == minP, region_start := min(subdata.2[, region_start])]
+        subdata.2[pval == minP, region_end := max(subdata.2[, region_end])]
+        subdata.2[pval == minP, NR_SNPs := sum(subdata.2[,NR_SNPs]) + nrow(subdata.2) - 1]
+        subdata.2[pval == minP, ]
+      }
+      subresult = rbindlist(subresult)
+      subresult[,region := paste(s2,region,sep="::")]
+      subresult
+    }
+    
+  }
+  result.32 = rbindlist(result.32,fill = T)
+  table(result.32$phenotype)
+  table(result.32$chr)
+  result.32
+  
+}
+result.3 = rbindlist(result.3,fill = T)
+table(result.3$phenotype,result.3$chr)
+result.3[, region := c(1:nrow(result.3))]
+result.3
+
+names(result.3)
+cols2keep = names(result.3)[c(22,20,21,16,1:9,19,10:13)]
+colsOut = setdiff(colnames(result.3), cols2keep)
+result.3[, get("colsOut") := NULL]
+setcolorder(result.3, cols2keep)
+setorder(result.3,chr,bp_hg19)
+result.3
+result.3[,.N,by=phenotype]
+
+save(result.3,file="../results/02_LociPerPhenotype.RData")
+
+#' # Locus collapsing over all phenotype ###
+#' *** 
+#' I want to know which loci appear in multiple settings, and how these collapsed loci would look like. 
+#' 
+subdata = copy(result.2)
+setorder(subdata, chr, bp_hg19)
+subset2 = unique(subdata$chr)
+
+result.4 = foreach(s2 = subset2) %do% {
+  # s2 = subset2[1]
+  subdata2 = copy(subdata)
+  subdata2 = subdata2[chr == s2, ]
+  
+  setkey(subdata2, bp_hg19)
+  
+  if(dim(subdata2)[1]<=1){
+    subdata2[, keep := T]
+    subdata2[, region := 1]
+    subdata2[, other_phenotypes := ""]
+    subdata2[, region := paste0("chr",s2,"::",region)]
+    
+  }else{
+    subdata2[, keep := NA]
+    subdata2[, region := as.numeric(NA)]
+    
+    subdata2[1, region := 1]
+    foreach(l = c(2:nrow(subdata2))) %do% {
+      if (subdata2[l, region_start] <= subdata2[l-1, region_end]) {
+        subdata2[l, region := subdata2[l-1, region]]
+      } else { subdata2[l, region := subdata2[l-1, region] + 1]}
+    }
+    
+    #keep best SNPs per region
+    allRegions = unique(subdata2[, region])
+    
+    subresult = foreach(r = allRegions) %do% {
+      subdata.2 = copy(subdata2)
+      subdata.2 = subdata.2[region == r, ]
+      minP = min(subdata.2[, pval])
+      subdata.2[pval == minP, region_start := min(subdata.2[, region_start])]
+      subdata.2[pval == minP, region_end := max(subdata.2[, region_end])]
+      subdata.2[pval == minP, NR_SNPs := sum(subdata.2[,NR_SNPs]) + nrow(subdata.2) - 1]
+      subdata.2[pval == minP, other_phenotypes := paste(unique(subdata.2[pval != minP, phenotype]), collapse = " | ")]
+      subdata.2[pval == minP, ]
+    }
+    subresult = rbindlist(subresult)
+    subresult[,region := paste0("chr",s2,"::",region)]
+    subresult
+  }
+}
+  
+result.4 = rbindlist(result.4,fill = T)
+result.4[,.N,by=phenotype]
+table(result.4$chr)
+result.4
+
+#' There is a total of 21 loci associated with at least one SNP. But I want to loci to be supported by at least two other variants!
+#' 
+result.4[NR_SNPs>1,]
+#' 
+#' There is a total of 13 loci associated with sufficient support. 
+#' 
+
+names(result.4)
+cols2keep = names(result.4)[c(22,20,21,16,23,1:9,19,10:13)]
+colsOut = setdiff(colnames(result.4), cols2keep)
+result.4[, get("colsOut") := NULL]
+setcolorder(result.4, cols2keep)
+result.4
+setorder(result.4,chr,bp_hg19)
+
+save(result.4,file="../results/02_LociOverallPhenotypes.RData")
+
+#' # Session Info ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
+
---
+++ scripts/03_InteractionTests.R
@@ -0,0 +1,296 @@
+#' ---
+#' title: "Interaction Tests"
+#' subtitle: "PCSK9 GWAMA sex-stratified"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' Perform series of interaction tests: 
+#' 
+#'  1. 3-way interaction test: SNP x sex x statin
+#'  2. 2-way interaction test: SNP x sex
+#'  3. 2-way interaction test: SNP x statin
+#' 
+#' # Initialize ####
+#' ***
+rm(list = ls())
+time0<-Sys.time()
+
+source("../SourceFile_forostar.R")
+.libPaths()
+setwd(paste0(projectpath_main,"/scripts/"))
+
+source("../helperFunctions/TwoWayInteractionTest_jp.R")
+source("../helperFunctions/ThreeWayInteractionTest_jp.R")
+source("../helperFunctions/getCorrelation.R")
+
+#' # Load data ####
+#' ***
+#' Load data and filter for suggestive significant and valid SNPs. Finally, merge all significant SNPs into one object. 
+#' 
+ToDoList = data.table(NR = 1:8)
+
+ToDoList[,statistic := list.files(path = "../data/",pattern = "PCSK9")]
+ToDoList[,statistic_path := paste0("../data/",statistic)]
+
+ToDoList[,pheno := gsub("SumStat_","",statistic)]
+ToDoList[,pheno := gsub("_230120.txt.gz","",pheno)]
+
+dumTab = foreach(i = 1:dim(ToDoList)[1])%do%{
+  #i=1
+  myRow = ToDoList[i,]
+  message("Working on data ",myRow$pheno)
+  data = fread(myRow$statistic_path)
+  data
+}
+
+result.0 = rbindlist(dumTab)
+
+cor_sex_combined = getCorrelation(data = result.0,pheno1 = "PCSK9_males",pheno2 = "PCSK9_females")
+cor_sex_free = getCorrelation(data = result.0,pheno1 = "PCSK9_males_free",pheno2 = "PCSK9_females_free")
+cor_sex_treated = getCorrelation(data = result.0,pheno1 = "PCSK9_males_treated",pheno2 = "PCSK9_females_treated")
+cor_statin_combined = getCorrelation(data = result.0,pheno1 = "PCSK9_treated",pheno2 = "PCSK9_free")
+cor_statin_males = getCorrelation(data = result.0,pheno1 = "PCSK9_males_treated",pheno2 = "PCSK9_males_free")
+cor_statin_females = getCorrelation(data = result.0,pheno1 = "PCSK9_females_treated",pheno2 = "PCSK9_females_free")
+corTab = rbind(cor_sex_combined,cor_sex_free,cor_sex_treated,
+               cor_statin_combined,cor_statin_males,cor_statin_females)
+corTab
+save(corTab, file = "../temp/03_CorrelationTable.RData")
+
+#' # Filter data ####
+#' ***
+#' I only want to check the 13 loci that I used in the GCTA and Credible Set Analyses. 
+#' 
+load("../results/02_LociOverallPhenotypes.RData")
+result.4 = result.4[NR_SNPs >1]
+
+SNPs_sig = result.0[invalidAssocs  == F & pval<=1e-6,unique(markername)]
+result.1 = copy(result.0)
+result.1 = result.1[markername %in% SNPs_sig,]
+
+result.4[,candidateGene := c("PCSK9","new1","MYNC","APOB","PLB1","SASH1","PRKAG2","new2","SLCO1B1",
+                             "NOS1","gene desert","TM6SF2,SUGP2", "MACROD2,SNRPB2")]
+
+result.1 = result.1[chr %in% result.4$chr,]
+
+result.2 = foreach(i=1:dim(result.4)[1])%do%{
+  #i=1
+  myRow = result.4[i,]
+  dummy = copy(result.1)
+  dummy = dummy[chr == myRow$chr]
+  dummy = dummy[bp_hg19 >= myRow$region_start]
+  dummy = dummy[bp_hg19 <= myRow$region_end]
+  dummy[,region := myRow$region]
+  dummy[,candidateGene := myRow$candidateGene]
+  dummy[,bestPheno := myRow$phenotype]
+  dummy
+}
+result.2 = rbindlist(result.2)
+result.1[markername %nin% result.2$markername,]
+table(result.2$phenotype,result.2$chr)
+setorder(result.2,chr,bp_hg19)
+
+#' # Get Strata info per SNP ####
+#' ***
+#' 
+result.2[,table(phenotype,bestPheno)]
+result.2[, bestSex := "combined"]
+result.2[grepl("_male",bestPheno), bestSex := "males"]
+result.2[grepl("_female",bestPheno), bestSex := "females"]
+result.2[,table(bestPheno,bestSex)]
+
+result.2[, bestStatin := "combined"]
+result.2[grepl("_free",bestPheno), bestStatin := "free"]
+result.2[grepl("_treated",bestPheno), bestStatin := "treated"]
+result.2[,table(bestPheno,bestStatin)]
+
+result.2[, sex := "combined"]
+result.2[grepl("_male",phenotype), sex := "males"]
+result.2[grepl("_female",phenotype), sex := "females"]
+result.2[,table(bestPheno,sex)]
+
+result.2[, statin := "combined"]
+result.2[grepl("_free",phenotype), statin := "free"]
+result.2[grepl("_treated",phenotype), statin := "treated"]
+result.2[,table(bestPheno,statin)]
+
+result.2[,corSex := corTab[1,correlation]]
+result.2[bestStatin == "free",corSex := corTab[2,correlation]]
+result.2[bestStatin == "treated",corSex := corTab[3,correlation]]
+result.2[,corStatin := corTab[4,correlation]]
+result.2[bestSex == "males",corStatin := corTab[5,correlation]]
+result.2[bestSex == "females",corStatin := corTab[6,correlation]]
+
+
+#' # Check EAFs ####
+#' ***
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_females_free",EAF])
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_females_treated",EAF])
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_males",EAF])
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_males_free",EAF])
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_males_treated",EAF])
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_free",EAF])
+plot(result.2[phenotype == "PCSK9_females",EAF],result.2[phenotype == "PCSK9_treated",EAF])
+
+save(result.2,file="../temp/03_IATest_input.RData")
+
+#' # 3-way Interaction Tests ####
+#' ***
+#' ## Run Test ####
+IATab_3way = ThreeWayInteractionTest_jp(data = result.2,
+                                  pheno1 = "PCSK9_males_treated",
+                                  pheno2 = "PCSK9_females_treated",
+                                  pheno3 = "PCSK9_males_free",
+                                  pheno4 = "PCSK9_females_free")
+
+matched = match(IATab_3way$markername,result.2$markername)
+table(is.na(matched))
+table(IATab_3way$markername == result.2$markername[matched])
+IATab_3way[,candidateGene := result.2$candidateGene[matched]]
+
+save(IATab_3way,file="../temp/03_IATest_output_3way.RData")
+
+#' ## Correct for multiple testing ####
+IATab_3way = IATab_3way[!is.na(IA_diff),]
+
+myFDR1 = addHierarchFDR(pvalues = IATab_3way$IA_pval, categs = IATab_3way$candidateGene,quiet = F)
+table(myFDR1$hierarch_fdr5proz,myFDR1$category)
+
+IATab_3way[,IA_pval_adj := myFDR1$fdr_level1]
+IATab_3way[,IA_hierarch_fdr5proz := myFDR1$hierarch_fdr5proz]
+IATab_3way[,table(IA_hierarch_fdr5proz,candidateGene)]
+IATab_3way[,table(IA_pval<0.05,candidateGene)]
+
+IATab_3way[,type2 := "unspecific"]
+IATab_3way[IA_pval<0.05,type2 := "nom. sig. interaction"]
+IATab_3way[IA_hierarch_fdr5proz==T,type2 := "interaction"]
+
+#' **Summary** 3-way interaction:
+#' 
+#' - **PCSK9**: 12 SNPs with nominal significant interactions 
+#'    - 8 at the beginning of the locus: gw sig for **treated males**, nom sig for free males and females, not sig for treated females
+#'    - 4 at the end of the locus: sug sig for **free females**, nom sig for treated males, not sig for treated females and free males
+#' - **MYNC**: 1 SNP with nominal significant interaction (sug sig in **free males**, not sig in the three other traits)
+#' - **SLCO1B1**: 214 SNPs with nominal significant interaction (sug sig in **free females**, not sig in the other three traits)
+#' - **SASH1**: 6 SNPs with significant interaction (sug sig in **treated females**, not sig in the other three traits)
+#'      
+
+save(IATab_3way,file = "../results/03_InteractionTests_3way.RData")
+
+#' # 2-way Interaction Tests ####
+#' ***
+#' The two way interaction should be tested with respect to the best-associated phenotype per loci. E.g. if the best-associated phenotype is statin-free females, then I compare the effects in statin-free males and females, and in statin-treated and statin-free females.  
+#' 
+#' ## Run Test for sex ####
+IATab_2way_sex = TwoWayInteractionTest_jp(data = result.2,
+                                          pheno1 = "males",
+                                          pheno2 = "females",
+                                          type = "sexIA",
+                                          corCol = "corSex")
+
+matched = match(IATab_2way_sex$markername,result.2$markername)
+table(is.na(matched))
+table(IATab_2way_sex$markername == result.2$markername[matched])
+IATab_2way_sex[,candidateGene := result.2$candidateGene[matched]]
+
+#' ## Correct for multiple testing for sex ####
+IATab_2way_sex = IATab_2way_sex[!is.na(IA_diff),]
+
+myFDR2 = addHierarchFDR(pvalues = IATab_2way_sex$IA_pval, categs = IATab_2way_sex$candidateGene,quiet = F)
+table(myFDR2$hierarch_fdr5proz,myFDR2$category)
+
+IATab_2way_sex[,IA_pval_adj := myFDR2$fdr_level1]
+IATab_2way_sex[,IA_hierarch_fdr5proz := myFDR2$hierarch_fdr5proz]
+IATab_2way_sex[,table(IA_hierarch_fdr5proz,candidateGene)]
+IATab_2way_sex[,table(IA_pval<0.05,candidateGene)]
+
+IATab_2way_sex[,type2 := "unspecific"]
+IATab_2way_sex[IA_hierarch_fdr5proz==T,type2 := "sex-interaction"]
+
+IATab_2way_sex[IA_hierarch_fdr5proz==T,.N,candidateGene]
+
+#' **Summary** 2-way sex interaction:
+#' 
+#' - **MYNC**: male-specific association
+#' - **PLB1**: female-specific association
+#' - **SASH1**: female-specific association
+#' - **PRKAG2**: male-specific association
+#' - **SLCO1B1**: female-specfic association
+#' - **gene desert**: male-specific association
+#' - **MACROD2,SNRPB2**: female-specfic association
+#' 
+
+IATab_2way_sex[IA_hierarch_fdr5proz==T & candidateGene %in% c("MYNC","PRKAG2","gene desert"),type2 := "male-specific"]
+IATab_2way_sex[IA_hierarch_fdr5proz==T & candidateGene %in% c("PLB1","SASH1","SLCO1B1","MACROD2,SNRPB2"),type2 := "female-specific"]
+table(IATab_2way_sex$type2)
+save(IATab_2way_sex,file="../temp/03_IATest_output_2way_sex.RData")
+
+
+#' ## Run Test for statin ####
+IATab_2way_statin = TwoWayInteractionTest_jp(data = result.2,
+                                             pheno1 = "treated",
+                                             pheno2 = "free",
+                                             type = "statinIA",
+                                             corCol = "corStatin")
+
+matched = match(IATab_2way_statin$markername,result.2$markername)
+table(is.na(matched))
+table(IATab_2way_statin$markername == result.2$markername[matched])
+IATab_2way_statin[,candidateGene := result.2$candidateGene[matched]]
+
+#' ## Correct for multiple testing for statin ####
+IATab_2way_statin = IATab_2way_statin[!is.na(IA_diff),]
+
+myFDR3 = addHierarchFDR(pvalues = IATab_2way_statin$IA_pval, categs = IATab_2way_statin$candidateGene,quiet = F)
+table(myFDR3$hierarch_fdr5proz,myFDR3$category)
+
+IATab_2way_statin[,IA_pval_adj := myFDR3$fdr_level1]
+IATab_2way_statin[,IA_hierarch_fdr5proz := myFDR3$hierarch_fdr5proz]
+IATab_2way_statin[,table(IA_hierarch_fdr5proz,candidateGene)]
+IATab_2way_statin[,table(IA_pval<0.05,candidateGene)]
+
+IATab_2way_statin[,type2 := "unspecific"]
+IATab_2way_statin[IA_hierarch_fdr5proz==T,type2 := "interaction"]
+
+IATab_2way_statin[IA_hierarch_fdr5proz==T,.N,candidateGene]
+
+#' **Summary** 2-way statin interaction:
+#' 
+#' - **PCSK9**: treatment-related association
+#' - **new1**: free-specific association
+#' - **SASH1**: treatment-specific association
+#' - **PRKAG2**: free-specific association
+#' - **gene desert**: treatment-specific association
+#' 
+IATab_2way_statin[IA_hierarch_fdr5proz==T & candidateGene %in% c("new1","PRKAG2"),type2 := "free-specific"]
+IATab_2way_statin[IA_hierarch_fdr5proz==T & candidateGene %in% c("SASH1","gene desert"),type2 := "treated-specific"]
+IATab_2way_statin[IA_hierarch_fdr5proz==T & candidateGene %in% c("PCSK9"),type2 := "treated-related"]
+
+table(IATab_2way_statin$type2)
+
+save(IATab_2way_statin,file="../temp/03_IATest_output_2way_statin.RData")
+
+#' ## Summary ####
+#' ***
+#' 
+#' * no interaction whatsoever: APOB, TM6SF2, NOS1
+#' * only sex interaction: PLB1, MYNC, MACROD2, SLCO1B1
+#' * sex and statin interaction: CYP51A1P3, PRKAG2, gene desert
+#' * sometimes interaction: PCSK9
+#' 
+IATab_2way = rbind(IATab_2way_sex,IATab_2way_statin)
+save(IATab_2way,file = "../results/03_InteractionTests_2way.RData")
+
+#' # Session Info ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME : " ,round(difftime(Sys.time(),time0,units = "mins"),3)," minutes")
+
