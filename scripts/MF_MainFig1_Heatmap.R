4832eeb4349999e60e7deea574cefa0a08a343e6
pottj <pott.ne.ja@gmail.com>
2023-09-11 19:03:16 +0200

    update main figures

--- scripts/MF_MainFig1_Heatmap.R
+++ scripts/MF_MainFig1_Heatmap.R
@@ -48,121 +48,83 @@ result.2[logP> -log10(1e-6), logP2 := 6]
 result.2[logP> -log10(5e-8), logP2 := 7.3]
 result.2[dumID2 %in% result.5$dumID2 & logP> -log10(5e-8), logP2 := 8]
 result.2[dumID2 %in% result.5$dumID2 & logP< -log10(5e-8), logP2 := 7]
+result.2[, logP3 := 0]
+result.2[logP> -log10(0.05), logP3 := 1.3]
+result.2[logP> -log10(1e-6), logP3 := 6]
+result.2[logP> -log10(5e-8), logP3 := 7.3]
+# result.2[dumID2 %in% result.5$dumID2 & logP> -log10(5e-8), comment := "bestPheno"]
+# result.2[dumID2 %in% result.5$dumID2 & logP< -log10(5e-8), comment := "bestPheno"]
 
 #' # Create Heatmap ####
 #' ***
-#' ## Normal beta ####
-plotData1<-dcast(result.2,
-                 formula = dumID1 ~ phenotype,
-                 value.var = c("beta"),
-                 sep = "_")
-
-SNPID = as.character(plotData1$dumID1)
-phenotypes = colnames(plotData1)[-1]
-phenotypes = gsub("PCSK9_","",phenotypes)
-phenotypes = gsub("females","F",phenotypes)
-phenotypes = gsub("males","M",phenotypes)
-phenotypes = gsub("_"," - ",phenotypes)
-
-plot.matrix1 = as.matrix(plotData1[, -1])
-rownames(plot.matrix1) = SNPID
-colnames(plot.matrix1) = phenotypes
-#plot.matrix1 = t(plot.matrix1)
-
-col_fun1 = colorRamp2(c(-0.4,0,0.06), c("coral","white","steelblue"))
-Heatmap(plot.matrix1, name = "beta",col=col_fun1)
-
-#' ## Positive beta ####
-plotData2<-dcast(result.2,
-                 formula = dumID1 ~ phenotype,
-                 value.var = c("beta2"),
-                 sep = "_")
-
-plot.matrix2 = as.matrix(plotData2[, -1])
-rownames(plot.matrix2) = SNPID
-colnames(plot.matrix2) = phenotypes
-
-col_fun2 = colorRamp2(c(0,0.4), c("white","steelblue"))
-Heatmap(plot.matrix2, name = "abs(beta)",col=col_fun2)
-
-#' ## Z score ####
-plotData3<-dcast(result.2,
-                 formula = dumID1 ~ phenotype,
-                 value.var = c("Zscore"),
-                 sep = "_")
-
-plot.matrix3 = as.matrix(plotData3[, -1])
-rownames(plot.matrix3) = SNPID
-colnames(plot.matrix3) = phenotypes
-#plot.matrix3 = t(plot.matrix3)
-
-col_fun3 = colorRamp2(c(-30,0,15), c("coral","white","steelblue"))
-Heatmap(plot.matrix3, name = "Zscore",col=col_fun3)
-
-#' ## Positive Z score ####
-plotData4<-dcast(result.2,
-                 formula = dumID1 ~ phenotype,
-                 value.var = c("Zscore2"),
-                 sep = "_")
-
-plot.matrix4 = as.matrix(plotData4[, -1])
-rownames(plot.matrix4) = SNPID
-colnames(plot.matrix4) = phenotypes
-#plot.matrix4 = t(plot.matrix4)
-
-col_fun4 = colorRamp2(c(0,30), c("white","steelblue"))
-Heatmap(plot.matrix4, name = "abs(Zscore)",col=col_fun4)
-
-#' ## Pvalue ####
-plotData5<-dcast(result.2,
-                 formula = dumID1 ~ phenotype,
-                 value.var = c("logP"),
-                 sep = "_")
-
-plot.matrix5 = as.matrix(plotData5[, -1])
-rownames(plot.matrix5) = SNPID
-colnames(plot.matrix5) = phenotypes
-#plot.matrix5 = t(plot.matrix5)
-
-col_fun5 = colorRamp2(c(0,1.3,6,7.3), c("white","yellow","orange","red"))
-Heatmap(t(plot.matrix5), name = "-logP",col=col_fun5)
-
-#' ## Pvalue 2 ####
 plotData6<-dcast(result.2,
                  formula = dumID1 ~ phenotype,
                  value.var = c("logP2"),
                  sep = "_")
 
+SNPID = as.character(plotData6$dumID1)
+phenotypes = colnames(plotData6)[-1]
+phenotypes = gsub("PCSK9_","",phenotypes)
+phenotypes = gsub("females","F",phenotypes)
+phenotypes = gsub("males","M",phenotypes)
+phenotypes = gsub("_"," - ",phenotypes)
+
 plot.matrix6 = as.matrix(plotData6[, -1])
 rownames(plot.matrix6) = SNPID
 colnames(plot.matrix6) = phenotypes
-#plot.matrix6 = t(plot.matrix6)
+plotData7<-dcast(result.2,
+                 formula = dumID1 ~ phenotype,
+                 value.var = c("logP3"),
+                 sep = "_")
+plot.matrix7 = as.matrix(plotData7[, -1])
+rownames(plot.matrix7) = SNPID
+colnames(plot.matrix7) = phenotypes
 
-col_fun6 = colorRamp2(c(0,1.3,6,7,7.3,8), c("#FFFFB2", "#FED976", "#FEB24C" ,"#FD8D3C", "#F03B20", "#BD0026"))
-Heatmap(plot.matrix6, name = "-logP",col=col_fun6)
+#resort phenotypes
+plot.matrix7 = plot.matrix7[,c(4,5,6,1,2,8,7,3)]
+plot.matrix6 = plot.matrix6[,c(4,5,6,1,2,8,7,3)]
 
-Heatmap(plot.matrix6, name = "-logP",col=col_fun6,show_column_names = FALSE, 
+#resort SNPs (as in Table 1)
+plot.matrix7 = plot.matrix7[c(9,11,8,10,2,5,3,4,14,6,12,1,13,7),]
+plot.matrix6 = plot.matrix6[c(9,11,8,10,2,5,3,4,14,6,12,1,13,7),]
+
+#col_fun6 = colorRamp2(c(0,1.3,6,7,7.3,8), c("#FFFFB2", "#FED976", "#FEB24C" ,"#FD8D3C", "#F03B20", "#BD0026"))
+col_fun6 = colorRamp2(c(0,1.3,6,7.3), c("#FFFFB2", "#FED976", "#FD8D3C",  "#BD0026"))
+Heatmap(plot.matrix7, name = "-logP",col=col_fun6)
+
+small_mat = plot.matrix6
+small_mat[small_mat==8] = "*"
+small_mat[small_mat==7] = "*"
+small_mat[small_mat!="*"] = ""
+
+Heatmap(plot.matrix7, name = "-logP",col=col_fun6,show_column_names = FALSE, 
         bottom_annotation = HeatmapAnnotation(text = anno_text(phenotypes, 
                                                                rot = 45, 
                                                                location = unit(1, "npc"), 
                                                                just = "right"),
-                                              annotation_height = max_text_width(phenotypes)))
+                                              annotation_height = max_text_width(phenotypes)),
+        cell_fun = function(j, i, x, y, width, height, fill) {
+          grid.text(small_mat[i, j], x, y, gp = gpar(fontsize = 15))})
 
-plot1 = Heatmap(plot.matrix6, name = "-logP",col=col_fun6, 
-                clustering_distance_rows="euclidean",
-                clustering_method_columns = "complete",
+
+plot1 = Heatmap(plot.matrix7, name = "-logP",col=col_fun6, 
+                cluster_rows = F, cluster_columns = F,
+                # clustering_distance_row = "euclidean",
+                # clustering_method_columns = "complete",
                 show_column_dend = FALSE, show_row_dend = FALSE,
                 show_heatmap_legend = F,show_column_names = FALSE, 
-                bottom_annotation = HeatmapAnnotation(text = anno_text(phenotypes, 
+                bottom_annotation = HeatmapAnnotation(text = anno_text(colnames(plot.matrix7), 
                                                                        rot = 45, 
                                                                        location = unit(1, "npc"), 
                                                                        just = "right"),
-                                                      annotation_height = max_text_width(phenotypes)))
+                                                      annotation_height = max_text_width(colnames(plot.matrix7))),
+                cell_fun = function(j, i, x, y, width, height, fill) {
+                  grid.text(small_mat[i, j], x, y, gp = gpar(fontsize = 15))})
 plot1
 
 #' # Save plot ####
 #' ***
-tiff(filename = "../figures/MainFigure1_Heatmap_logp.tiff",
+tiff(filename = "../figures/MainFigure1_Heatmap_logp_230904.tiff",
      width = 1200, height = 1400, res=250, compression = 'lzw')
 plot1
 dev.off()
---
+++ scripts/MF_MainFig2_IAPlot.R
@@ -0,0 +1,87 @@
+#' ---
+#' title: "Main Figure 2: 3-way and 2-way interaction plots"
+#' subtitle: "PCSK9 GWAMA sex-stratified"
+#' author: "Janne Pott"
+#' date: "Last compiled on `r format(Sys.time(), '%d %B, %Y')`"
+#' output:
+#'   html_document:
+#'     toc: true
+#'     number_sections: true
+#'     toc_float: true
+#'     code_folding: show
+#' ---
+#'
+#' # Introduction ####
+#' ***
+#' **Figure 2: Plot of differences**
+#' 
+#' I want some sort of panel plot (facets)
+#' 
+#' - A) 3-way interaction (x=b_(F,treated)-b_(M,treated), y=b_(F,free)-b_(M,free))
+#' - B) 2-way sex interaction (x=b_M, y =b_F)
+#' - C) 2-way statin interaction (x=b_treated, y=b_free)
+#' 
+#' # Initialize ####
+#' ***
+rm(list = ls())
+time0 = Sys.time()
+
+source("../SourceFile_angmar.R")
+
+setwd(paste0(projectpath_main,"scripts/"))
+
+#' # Load and prep data ####
+#' ***
+load("../results/04_IATest_2way_complete.RData")
+
+IATab[,myX := trait1_beta]
+IATab[,myY := trait2_beta]
+IATab[,myX_SE := trait1_SE]
+IATab[,myY_SE := trait2_SE]
+IATab[,rsID := gsub(":.*","",markername)]
+IATab[,myLabel := "x - not sig"]
+IATab[IA_hierarch_fdr5proz == T,myLabel := paste0(candidateGene," \n",rsID," \n")]
+
+
+myPlot1 = ggplot(IATab, aes(x=myX, y=myY, color=myLabel, alpha = IA_hierarch_fdr5proz)) +
+  facet_wrap(~type, nrow = 1, 
+             strip.position = "left", 
+             labeller = as_labeller(c("sexIA" = "SNP effect in females", 
+                                      "statinIA" = "SNP effect in statin-free") ) )+
+  geom_hline(yintercept = 0, color="grey", linetype="dashed", size=1.15)+
+  geom_vline(xintercept = 0, color="grey", linetype="dashed", size=1.15)+
+  geom_abline(intercept = 0,slope=1,color="grey", linetype="dashed", size=1.15)+
+  geom_point(size=2)+ 
+  geom_errorbar(aes(ymin = myY- 1.96*myY_SE, ymax = myY+ 1.96*myY_SE)) +
+  geom_errorbarh(aes(xmin = myX- 1.96*myX_SE, xmax = myX + 1.96*myX_SE)) +
+  theme_bw(base_size = 10) + 
+  scale_colour_manual(values=c(brewer.pal(10, "Paired"),"#000000"))+
+  scale_alpha_manual(values = c(0.3,1),labels = c("not significant", "significant"))+
+  theme(plot.title = element_text(hjust = 0, size=22,face="bold"),
+        axis.title.x = element_text(size=14,face="bold"),
+        axis.title.y = element_text(size=14,face="bold"),
+        axis.text = element_text(size=12,face="bold"),
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        strip.text = element_text(size = 14, face = "bold"),
+        legend.title = element_text(size = 12),
+        legend.text = element_text(size = 10))+
+  ylab(NULL) +
+  guides(alpha = NULL, size = NULL)+
+  labs(x="SNP effect in males                                                      SNP effect in statin-treated", 
+       color="", alpha="") 
+
+myPlot1
+
+#' # Save plots ####
+#' ***
+tiff(filename = "../figures/MainFigure2_InteractionScatterPlot_230908.tiff",
+     width = 2250, height = 1125, res=200, compression = 'lzw')
+myPlot1
+dev.off()
+
+#' # Sessioninfo ####
+#' ***
+sessionInfo()
+message("\nTOTAL TIME of script (in minutes): " ,round(difftime(Sys.time(), time0, tz,units = "mins"),2))
+
